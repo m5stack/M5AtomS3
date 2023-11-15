5926c925d7b2a5f121be88fa23eb0d9a430dc0d6
Svelbeard <56926365+Svelbeard@users.noreply.github.com>
2023-11-15 23:22:33 +0000

    Prevent naming conflict

--- src/utility/In_eSPI_Drivers/ST7789_Defines.h
+++ src/utility/In_eSPI_Drivers/ST7789_Defines.h
@@ -127,22 +127,22 @@
 #define ST7789_NVMSET    0xFC  // NVM setting
 #define ST7789_PROMACT   0xFE  // Program action
 
-#define BLACK       0x0000 /*   0,   0,   0 */
-#define NAVY        0x000F /*   0,   0, 128 */
-#define DARKGREEN   0x03E0 /*   0, 128,   0 */
-#define DARKCYAN    0x03EF /*   0, 128, 128 */
-#define MAROON      0x7800 /* 128,   0,   0 */
-#define PURPLE      0x780F /* 128,   0, 128 */
-#define OLIVE       0x7BE0 /* 128, 128,   0 */
-#define LIGHTGREY   0xC618 /* 192, 192, 192 */
-#define DARKGREY    0x7BEF /* 128, 128, 128 */
-#define BLUE        0x001F /*   0,   0, 255 */
-#define GREEN       0x07E0 /*   0, 255,   0 */
-#define CYAN        0x07FF /*   0, 255, 255 */
-#define RED         0xF800 /* 255,   0,   0 */
-#define MAGENTA     0xF81F /* 255,   0, 255 */
-#define YELLOW      0xFFE0 /* 255, 255,   0 */
-#define WHITE       0xFFFF /* 255, 255, 255 */
-#define ORANGE      0xFD20 /* 255, 165,   0 */
-#define GREENYELLOW 0xAFE5 /* 173, 255,  47 */
-#define PINK        0xF81F
+#define BLACKATM       0x0000 /*   0,   0,   0 */
+#define NAVYATM        0x000F /*   0,   0, 128 */
+#define DARKGREENATM   0x03E0 /*   0, 128,   0 */
+#define DARKCYANATM    0x03EF /*   0, 128, 128 */
+#define MAROONATM      0x7800 /* 128,   0,   0 */
+#define PURPLEATM      0x780F /* 128,   0, 128 */
+#define OLIVEATM       0x7BE0 /* 128, 128,   0 */
+#define LIGHTGREYATM   0xC618 /* 192, 192, 192 */
+#define DARKGREYATM    0x7BEF /* 128, 128, 128 */
+#define BLUEATM        0x001F /*   0,   0, 255 */
+#define GREENATM       0x07E0 /*   0, 255,   0 */
+#define CYANATM        0x07FF /*   0, 255, 255 */
+#define REDATM         0xF800 /* 255,   0,   0 */
+#define MAGENTAATM     0xF81F /* 255,   0, 255 */
+#define YELLOWATM      0xFFE0 /* 255, 255,   0 */
+#define WHITEATM       0xFFFF /* 255, 255, 255 */
+#define ORANGEATM      0xFD20 /* 255, 165,   0 */
+#define GREENYELLOWATM 0xAFE5 /* 173, 255,  47 */
+#define PINKATM        0xF81F
